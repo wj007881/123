db70aeb1aa503fb03c4ab05e505d045bffe217a5
嗷呜嗷呜 <425547052@qq.com>
2017-04-25 17:32:40 +0800

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,124 @@
+/**
+ * Created by Administrator on 2016/9/29 0029.
+ */
+var  data= [
+                {
+                    cpuname:'i7 7700k',
+					gpuname:'GTX 1080Ti',
+					ypname:'希捷Barracuda 1TB ',
+					ncname:'海岛船 8G DDR4',
+					zbname:'微星Z270',
+					jxname:'IN WIN S-Frame',
+                    num:1,
+                    price:15800,
+                    path:'img/ss.png',
+                },
+                {
+                    cpuname:'i5 7500',
+					gpuname:'GTX 1050Ti',
+					ypname:'西部数据1TB',
+					ncname:'金士顿 8G DDR4',
+					zbname:'技嘉GA-B150M-D3H',
+					jxname:'金河田三角洲',
+                    num:1,
+                    price:9500,
+                    path:'img/ss.png',
+                },
+                {
+                    cpuname:'i3 7300',
+					gpuname:'GTX 950Ti',
+					ypname:'东芝P300系列 1TB',
+					ncname:'金士顿 4G DDR4',
+					zbname:'昂达P150S-D4',
+					jxname:'海盗船300R侧透版',
+                    num:1,
+                    price:5600,
+                    path:'img/ss.png',
+                },
+				 {
+                    cpuname:'奔腾G4560',
+					gpuname:'AMD R470D',
+					ypname:'三星M9T 2TB',
+					ncname:'金士顿 4G DDR3',
+					zbname:'华硕EX-B150M-V5',
+					jxname:'至睿创客TX12',
+                    num:1,
+                    price:2000,
+                    path:'img/ss.png',
+                },
+				 {
+                    cpuname:'奔腾G4560',
+					gpuname:'AMD R470D',
+					ypname:'三星M9T 2TB',
+					ncname:'金士顿 4G DDR3',
+					zbname:'华硕EX-B150M-V5',
+					jxname:'至睿创客TX12',
+                    num:1,
+                    price:2000,
+                    path:'img/ss.png',
+                },
+				 {
+                    cpuname:'奔腾G4560',
+					gpuname:'AMD R470D',
+					ypname:'三星M9T 2TB',
+					ncname:'金士顿 4G DDR3',
+					zbname:'华硕EX-B150M-V5',
+					jxname:'至睿创客TX12',
+                    num:1,
+                    price:2000,
+                    path:'img/ss.png',
+                }
+            ];
+		
+var parts = [
+			{cpuname:'奔腾G4560',
+			 gpuname:'AMD R470',
+			 ncname:'金士顿 4G DDR4',
+			 },
+			{cpuname:'酷睿I7 4790k',
+			 gpuname:'GTX1050',
+			 ncname:'金士顿 8G DDR4'
+			 },
+			{cpuname:'酷睿I5 4590',
+			 gpuname:'GTX1060',
+			 ncname:'海岛船 8G DDR4',
+			 },
+			{cpuname:'酷睿I3 7300',
+			 gpuname:'AMD R480',
+			 ncname:'金士顿 4G DDR3',
+			 },
+				
+				];
+				
+var zixun = [
+            {biaoti:"全面进化的北极星 Radeon RX 580首测",
+			 jian:"盼望着，盼望着，春天来了，Vega的脚步又近了。然而据小道消息称，Vega最早也要在5月份才能与大家见面，为了安抚A饭们迫切的心情，AMD在4月中旬的这个时候正式为大家带来了Polaris 10的升级版产品——Radeon RX 580",
+			 path:"rx580.html",
+			 imgpath:"img/RX580.jpg",
+				},
+			{biaoti:"推翻现有市场格局！Ryzen影响力有多大",
+			 jian:"2017年3月2日晚上10点整，锐龙 AMD Ryzen 7系列处理器正式在全球范围内解禁，这句话的意思是，玩家可以通过线下或者线上的渠道购买到这个系列的产品了，作为国内专业的科技媒体，我们ZOL也早在Ryzen 7系列产品解禁之前就收到了AMD送测的锐龙 AMD Ryzen 7处理器了。",
+			 path:"ruilong.html",
+			 imgpath:"img/锐龙.jpg",
+				},
+			{biaoti:"27寸2K优质面板 明基EW2770QZ显示器",
+			 jian:"在电竞大趋势的促使下，很多显示器厂商都上市了与电竞结合的相关产品。越来越高的刷新率、越做越大的显示尺寸、越做越精的显示分辨率，但长时间使用后，往往人们早已忘记了液晶显示器最早诞生的初衷，或许那就是“护眼”。",
+			 path:"显示器.html",
+			 imgpath:"img/显示器.jpg",
+				},
+			{biaoti:"帮同学装机却不听劝 结果被坑2000元",
+			 jian:"由于笔者的工作性质就是和电脑打交道，所以经常会有一些亲朋好友找笔者修电脑或者装机。这不，最近就有一名同学希望让我帮他装一台电脑，不过他却不听我的劝告，最终被坑了大约2000元，下面我们就来一起看看事情的经过。",
+			 path:"bang.html",
+			 imgpath:"img/bang.jpg",
+				},
+			{biaoti:"真拆实料：SKY鼠标被山寨竟月销过万?!",
+			 jian:"你有没有见过这样的鼠标，它的市售价非常低廉，而且有着非常可观的销量，在销售页面下方产品介绍处也有高于同价格品类产品的“配置”。看似一切都是那么“美好”，但“似曾相识”的鼠标外观也未免太尴尬。",
+			 path:"shubiao.html",
+			 imgpath:"img/shubiao.jpg",
+				},
+
+
+
+]
+
+			
